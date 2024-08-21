83df31ad58728a5ac9ffeaa46f3d2eec79649151
dscotta <166068238+dscotta@users.noreply.github.com>
2024-08-21 17:09:17 -0300

    Create metrics.sh
    asd

---
+++ metrics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Display CPU usage
+echo "CPU Usage:"
+top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2 + $4 "%"}'
+
+# Display Memory usage
+echo "Memory Usage:"
+free -m | awk 'NR==2{printf "Used Memory: %sMB (%.2f%%)\n", $3, $3*100/$2 }'
