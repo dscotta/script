0f42e7947ad8736f49c55bd200ae553113c25242
dscotta <166068238+dscotta@users.noreply.github.com>
2024-08-21 17:35:35 -0300

    Update metrics.sh
    1

--- metrics.sh
+++ metrics.sh
@@ -7,4 +7,3 @@ top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2 + $4 "%"}'
 # Display Memory usage
 echo "Memory Usage:"
 free -m | awk 'NR==2{printf "Used Memory: %sMB (%.2f%%)\n", $3, $3*100/$2 }'
-hostname
